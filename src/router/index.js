c9f8c93c4d48bd072716b0e06c6e15c7106f975d
Sidraque <sidraqueagostinho150@gmail.com>
2024-05-23 20:59:16 -0300

    Corrigir dependências e caminhos de importação

--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Clientes from '../components/Clientes.vue'
 import Login from '../components/Login.vue'
-import Pedidos from '../components/Pedidos.vue'
-import Produtos from '../components/Produtos.vue'
+//import Pedidos from '../components/Pedidos.vue'
+//import Produtos from '../components/Produtos.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Login },
   { path: '/clientes', component: Clientes },
-  { path: '/pedidos', component: Pedidos },
-  { path: '/produtos', component: Produtos }
+  //{ path: '/pedidos', component: Pedidos },
+  //{ path: '/produtos', component: Produtos }
 ]
 
 const router = new VueRouter({
